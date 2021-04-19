882a29f337b9d16c4c8b5d93c76b34b1502de979
Fernando Pioli <fernandohrp@gmail.com>
2021-04-19 07:26:24 -0300

    test: login returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -47,5 +47,13 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('should return 401 on login', async () => {
+      await request(app).post('/api/login')
+        .send({
+          email: 'fernandohrp@gmail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
