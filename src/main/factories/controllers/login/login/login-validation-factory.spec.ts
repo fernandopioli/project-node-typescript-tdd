b11920e43dcf29ff573cf67040b83c8683eb1c0d
Fernando Pioli <fernandohrp@gmail.com>
2021-06-06 12:28:46 -0300

    refactor: set jest mock to use module alias as well

--- src/main/factories/controllers/login/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login/login-validation-factory.spec.ts
@@ -4,7 +4,7 @@ import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '@
 import { EmailValidator } from '@/validation/protocols/email-validator'
 import { Validation } from '@/presentation/protocols/validation'
 
-jest.mock('../../../../../validation/validators/validation-composite')
+jest.mock('@/validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
